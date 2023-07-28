3dcd7ed2a4f64544d267f0181c087baca8c06694
priyababu07 <priyababu312@gmail.com>
2023-07-28 17:32:30 +0530

    few

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.vue"
 import Image from "./components/Image.vue"
 import Quote from "./components/Quote.vue"
 import About from "./components/About.vue"
+
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import About from "./components/About.vue"
 <Quote/>
 <CardContainer/>
 <About/>
+
 </template>
 
 <style scoped>
--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="container">
       <div class="message-bubble">
-        
-        
-        <p class="subtitle">Pillzen offers an effortless <br> medication tracking,<br> aherence,communication,<br> and centralized <br> medical informtion.</p>
-        
+        <p class="subtitle">Pillzen offers an effortless <br> medication tracking,<br> adherence, communication,<br> and centralized <br> medical information.</p>
       </div>
-    
-    <div class="image-container">
+  
+      <div class="paragraph">
+        <p>"Like education, health also needs <br>to be given importance"<br>-Shiv Nadar</p>
+      </div>
+  
+      <div class="image-container">
         <img src="/assets/PhotoFunia-1689611954 1.png" alt="Your Image" class="image" />
       </div>
     </div>
-
   </template>
   
   <script setup>
@@ -28,7 +28,7 @@
   }
   
   .message-bubble {
-    background-color:rgb(12, 168, 147);
+    background-color: rgb(12, 168, 147);
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
     border-radius: 10px;
     padding: 1.5rem;
@@ -36,12 +36,9 @@
     color: rgb(15, 14, 14);
     position: relative;
     max-width: 500px;
-    
-    margin-bottom: 300px; 
-    margin-right: 70%;
+    margin-right: 270px;
+    margin-bottom: 300px;
   }
-  
-
   
   .message-bubble::before {
     content: '';
@@ -58,20 +55,42 @@
     margin-bottom: 1rem;
     font-weight: 200;
   }
-
+  
+  .paragraph {
+    font-family: 'Dancing Script', cursive;
+    margin-right: 50px;
+    margin-bottom: 300px;
+    font-size: 25px;
+  }
+  
   .image-container {
-  margin-bottom:300px ;
-  padding-left:1rem; /* Add some spacing between the image and the message content */
-}
+    margin-bottom: 330px;
+    padding-left: 1rem; /* Add some spacing between the image and the message content */
+  }
+  
+  .image {
+    width: 150px; /* Adjust the width of the image as per your requirement */
+    height: auto;
+    border-radius: 10px; /* Add border-radius to make the image rounded */
+  }
+  
+    @media (max-width: 768px) {
+        .container {
+    flex-direction: column; /* Stack the elements in one after another downward manner */
+  }
 
-.image {
-  width: 150px; /* Adjust the width of the image as per your requirement */
-  height: auto;
-  border-radius: 10px; /* Add border-radius to make the image rounded */
-}
-  
-  
+  .message-bubble {
+    max-width: 90%; /* Set the message bubble width to adjust for smaller screens */
+    margin-bottom: 15px; /* Adjust the margin for better spacing */
+    margin-right: 0;
+    margin-top: 15px;
+  }
+
+  .paragraph {
+    font-size: 18px; /* Adjust the font size for better readability on small screens */
+    margin-bottom: 15px; /* Adjust the margin for better spacing */
+  }
+  }
+
   </style>
-  
-  
   

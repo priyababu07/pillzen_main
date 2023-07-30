e03e9e07fbfd763cbf8872a632e3adcb9af80a86
rahulbabu7 <rahulbabu436@gmail.com>
2023-07-30 10:23:28 +0530

    added card to center

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,33 +1,29 @@
 <template>
+  <br>
+  <div class="container" id="services">
     <br>
-    <div class="container" id="services">
-  
-      <br>
-      <div class="row g-3 justify-content-center">  
-  
-        <div class="card">
-          <h2>Our Goal</h2>
-          <br>
-          <p>Interactions with patients, clinical diagnoses, and potential treatments can be augmented and documented more accurately and in near real-time</p>
-        </div>
-  
-        <div class="card">
-          <h2>Our Vision</h2>
-          <br>
-          <p>To develop a safe world with health and wealth. To help in the development of effective and interpersonal relationship skills with patients, doctors, and colleagues</p>
-        </div>
-  
-        <div class="card">
-          <h2>Our Mission</h2>
-          <br>
-          <p>Managing one's own medicines is a skill that is necessary for successful independent living.</p>
-        </div>
-  
+    <div class="row g-3 justify-content-center">
+      <div class="card">
+        <h2>Our Goal</h2>
+        <br>
+        <p>Interactions with patients, clinical diagnoses, and potential treatments can be augmented and documented more accurately and in near real-time</p>
+      </div>
+
+      <div class="card">
+        <h2>Our Vision</h2>
+        <br>
+        <p>To develop a safe world with health and wealth. To help in the development of effective and interpersonal relationship skills with patients, doctors, and colleagues</p>
+      </div>
+
+      <div class="card">
+        <h2>Our Mission</h2>
+        <br>
+        <p>Managing one's own medicines is a skill that is necessary for successful independent living.</p>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
-  
 <style scoped>
 .card {
   box-shadow: 0px 30px 30px -4px rgba(0, 0, 0, 0.1), 0px 4px 8px -4px rgba(0, 0, 0, 0.3);
@@ -63,14 +59,16 @@ h2 {
 }
 
 @media (max-width: 768px) {
-  .card:hover {
-    transform: translateY(-10px);
-    cursor: pointer;
-    background-color: rgb(12, 168, 147);
-  }
-  .card{
+  .card {
     display: flex;
     flex-direction: column;
+    justify-content: center; /* Center the cards vertically */
+    align-items: center; /* Center the cards horizontally */
+    margin-right: 0; /* Remove the right margin on smaller screens to ensure cards are in the same row */
+  }
+
+  .row.g-3 {
+    gap: 0; /* Remove the gap between cards on smaller screens */
   }
 
   h2 {
@@ -79,13 +77,16 @@ h2 {
     font-weight: bold;
     margin-top: 20px;
   }
+
   .card:hover {
-  transform: translateY(-10px);
-  cursor: pointer;
-  background-color: rgb(12, 168, 147);
-}
+    transform: translateY(-10px);
+    cursor: pointer;
+    background-color: rgb(12, 168, 147);
+  }
+
+  #card {
+    display: flex;
+    justify-content: center;
+  }
 }
 </style>
-   
-  
-  

355807b147c701e44fe31a690f4e4c06df4e6860
rahulbabu7 <rahulbabu436@gmail.com>
2023-07-30 11:02:49 +0530

    added

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -3,16 +3,14 @@
     <div class="message-bubble">
       <p class="subtitle">Pillzen offers an effortless medication tracking,<br> adherence, communication,<br> and centralized <br> medical information.</p>
     </div>
-<section>
-  <div class="paragraph">
-      <p>"Like education, health also needs to be given importance"<br>-Shiv Nadar</p>
-    </div>
-
-    <div class="image-container">
-      <img src="/assets/PhotoFunia-16896119541.png" alt="Your Image" class="image" />
-    </div>
-</section>
-   
+    <section>
+      <div class="paragraph">
+        <p>"Like education, health also needs to be given importance"<br>-Shiv Nadar</p>
+      </div>
+      <div class="image-container">
+        <img src="/assets/PhotoFunia-16896119541.png" alt="Your Image" class="image" />
+      </div>
+    </section>
   </div>
 </template>
 
@@ -21,12 +19,13 @@
 </script>
 
 <style scoped>
+ @import url('https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap');
 .container {
   /* min-height: 100vh; */
   width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: center; /* Center the content horizontally */
+  align-items: center; /* Center the content vertically */
 }
 
 .message-bubble {
@@ -38,9 +37,8 @@
   color: rgb(15, 14, 14);
   position: relative;
   max-width: 200px;
-  margin-right: 270px;
+  margin-right: 100px;
   margin-top: 50px;
-  
 }
 
 .message-bubble::before {
@@ -60,11 +58,13 @@
 }
 
 .paragraph {
-  font-family:'Dancing Script',cursive;
-  
-  margin-right: 50px;
+  font-family: 'Cedarville Cursive', cursive;
+  /* margin-right: 50px; */
+  padding-right: 10px;
   /* margin-bottom: 300px; */
   font-size: 25px;
+  text-align: center; /* Center the paragraph text */
+  padding-top: 50px;
 }
 
 .image-container {
@@ -77,35 +77,35 @@
   height: auto;
   border-radius: 10px; /* Add border-radius to make the image rounded */
 }
-section{
+
+section {
   display: flex;
   flex-direction: row;
 }
 
-  @media (max-width: 768px) {
-      .container {
-  flex-direction: column; /* Stack the elements in one after another downward manner */
-}
+@media (max-width: 768px) {
+  .container {
+    flex-direction: column; /* Stack the elements in one after another downward manner */
+  }
 
-.message-bubble {
-  max-width: 60%; /* Set the message bubble width to adjust for smaller screens */
-  margin-bottom: 15px; /* Adjust the margin for better spacing */
-  margin-right: 0;
-  margin-top: 15px;
-  max-height: 100%;
-}
+  .message-bubble {
+    max-width: 60%; /* Set the message bubble width to adjust for smaller screens */
+    margin-bottom: 15px; /* Adjust the margin for better spacing */
+    margin-right: 0;
+    margin-top: 15px;
+    max-height: 100%;
+  }
 
-.paragraph {
-  font-size: 18px; /* Adjust the font size for better readability on small screens */
-  margin-bottom: 15px; /* Adjust the margin for better spacing */
-}
+  .paragraph {
+    font-size: 18px; /* Adjust the font size for better readability on small screens */
+    margin-bottom: 15px; /* Adjust the margin for better spacing */
+  }
 
-section{
-  display: flex;
-  flex-direction: row;
-  margin-left: 10px;
-  margin-top: 20px;
+  section {
+    display: flex;
+    flex-direction: row;
+    margin-left: 10px;
+    margin-top: 20px;
+  }
 }
-}
-
 </style>
